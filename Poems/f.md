314e63d0aeae4c6dbe9483d8e29c7dc3983822b1
Jayzyzy <Jayzyzy@outlook.com>
2022-09-05 07:29:37 +0800

    d

---
+++ Poems/f.md
@@ -0,0 +1,3 @@
+
+do you ever dream something
+do you want to achieve something
--- py/02.py
+++ py/02.py
@@ -4,7 +4,8 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 getting=rq.get('http://star.sse.com.cn/star/renewal/',headers=headers)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47'}
 
-g2=rq.get('http://kcb.sse.com.cn/renewal/xmxq/index.shtml?auditId=632&anchor_type=0',headers=headers)
+# g2=rq.get('http://kcb.sse.com.cn/renewal/xmxq/index.shtml?auditId=632&anchor_type=0',headers=headers)
+g2 = rq.get('https://eams.shanghaitech.edu.cn/eams/courseTableForStd!courseTable.action',headers=headers)
 # g2=g2.text
 print(getting)
 print(g2)
@@ -17,7 +18,7 @@ g3=rq.get('https://www.zhihu.com/question/29717275',headers=headers)
 
 print(g3)
 
-u=open('gitpa/02.html','w',encoding="utf-8")
+u=open('html/zh.html','w',encoding="utf-8")
 
 u.write(g2)
 u.close()
